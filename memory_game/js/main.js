24a5426d6ea365a19d206c153b3aebc4f83e6acf
BryanG <BryanG@Bryans-MacBook-Pro.local>
2019-08-09 09:39:20 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,12 +1,25 @@
-var cardOne = "queen";
-var cardTwo = "queen";
 
-var cardThree = "king";
-var cardFour= "king";
+alert('Hello, friends.');
+const cards = [ 'queen' , 'queen' , 'king', 'king'];
+const cardsInPlay = [];
 
+const cardOne = cards[0];
+const cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if ( cardsInPlay.length === 2 ) {
+
+    if ( cardsInPlay[0] === cardsInPlay[2]) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
+}
 
 
 
 console.log("User flipped " + cardOne);
 
-console.log("User flipped " + cardFour);
+console.log("User flipped " + cardTwo);
